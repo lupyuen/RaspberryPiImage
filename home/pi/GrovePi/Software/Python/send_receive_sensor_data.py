4a00d7c0078724d6f8131836ad6cf4300a26d0c8
lupyuen <lupyuen@gmail.com>
2015-10-27 01:41:14 +0800

    Actuate the LED

--- home/pi/GrovePi/Software/Python/send_receive_sensor_data.py
+++ home/pi/GrovePi/Software/Python/send_receive_sensor_data.py
@@ -5,7 +5,7 @@
 # (http://www.seeedstudio.com/wiki/Grove_-_Temperature_Sensor)
 
 import time
-# import grovepi
+import grovepi
 import datetime
 import ssl
 import json
@@ -27,6 +27,33 @@ temp_sensor = 1
 sound_sensor = 2
 isConnected = False
 
+# Connect the Grove LED to digital port D4
+led = 4
+grovepi.pinMode(led, "OUTPUT")
+time.sleep(1)
+
+
+# Control my actuators based on the specified attribute and value, e.g. "led=on" will switch on my LED.
+def actuate(attribute, value):
+    if attribute == "timestamp":
+        # Ignore the timestamp attribute, it's only for info.
+        return
+    print("Setting " + attribute + " to " + value + "...")
+    if attribute == "led":
+        # We actuate the LED for "on", "off" or "flash1".
+        if value == "on":
+            # Switch on LED.
+            grovepi.digitalWrite(led, 1)
+        elif value == "off":
+            # Switch off LED.
+            grovepi.digitalWrite(led, 0)
+        elif value == "flash1":
+            # Switch on LED, wait 1 second, switch it off.
+            grovepi.digitalWrite(led, 1)
+            time.sleep(1)
+            grovepi.digitalWrite(led, 0)
+            time.sleep(1)
+
 
 # This is called when we are connected to AWS IoT via MQTT.
 def on_connect(client2, userdata, flags, rc):
@@ -34,8 +61,9 @@ def on_connect(client2, userdata, flags, rc):
     global isConnected
     isConnected = True
     # Subscribe to our MQTT topic so that we will receive notifications of updates.
-    # client2.subscribe("$aws/things/" + deviceName + "/shadow/update")
-    client2.subscribe("$aws/things/" + deviceName + "/shadow/update/accepted")
+    topic = "$aws/things/" + deviceName + "/shadow/update/accepted"
+    print("Subscribing to MQTT topic " + topic)
+    client2.subscribe(topic)
 
 
 # This is called when we receive a subscription notification from AWS IoT.
@@ -43,15 +71,16 @@ def on_message(client2, userdata, msg):
     # Convert the JSON payload to a Python dictionary.
     # The payload is in binary format so we need to decode as UTF-8.
     payload2 = json.loads(msg.payload.decode("utf-8"))
-    print("Received message, topic: " + msg.topic + ", payload: " +
+    print("Received message, topic: " + msg.topic + ", payload:\n" +
           json.dumps(payload2, indent=4, separators=(',', ': ')))
+    # If there is a desired state in this message, then we actuate, e.g. if we see "led=on", we switch on the LED.
     if payload2.get("state") is not None and payload2["state"].get("desired") is not None:
+        # Get the desired state and loop through all attributes inside.
         desiredState = payload2["state"]["desired"]
         for attribute in desiredState:
-            if attribute == "timestamp":
-                continue
+            # We handle the attribute and desired value by actuating.
             value = desiredState.get(attribute)
-            print("set " + attribute + " to " + value)
+            actuate(attribute, value)
 
 
 # Print out log messages for tracing.
@@ -85,22 +114,25 @@ while True:
             continue
 
         # Read sensor values. Prepare our sensor data in JSON format.
-        payload = json.dumps({
+        payload = {
             "state": {
                 "reported": {
-                    # "temperature": round(grovepi.temp(temp_sensor, '1.1'), 1),
-                    "temperature": round(29.1, 1),
+                    # "temperature": round(29.1, 1),
+                    "temperature": round(grovepi.temp(temp_sensor, '1.1'), 1),
+                    "light_level": grovepi.analogRead(light_sensor),
+                    "sound_level": grovepi.analogRead(sound_sensor),
                     "timestamp": datetime.datetime.now().isoformat()
                 }
             }
-        })
-        print("Sending sensor data to AWS IoT: ", payload)
+        }
+        print("Sending sensor data to AWS IoT...\n" +
+              json.dumps(payload, indent=4, separators=(',', ': ')))
 
         # Publish our sensor data to AWS IoT via the MQTT topic, also known as updating our "Thing Shadow".
-        client.publish("$aws/things/" + deviceName + "/shadow/update", payload)
+        client.publish("$aws/things/" + deviceName + "/shadow/update", json.dumps(payload))
         print("Sent to AWS IoT")
 
-        # Wait 10 seconds before sending the next set of sensor data.
+        # Wait 30 seconds before sending the next set of sensor data.
         time.sleep(30)
 
     except KeyboardInterrupt:
