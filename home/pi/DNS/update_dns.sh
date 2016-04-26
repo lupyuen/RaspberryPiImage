425d76ce802feb23d5589df2894e487d4d9679b3
Lee Lup Yuen <lupyuen@gmail.com>
2016-04-26 11:39:55 +0800

    Update update_dns.sh

--- home/pi/DNS/update_dns.sh
+++ home/pi/DNS/update_dns.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-##  This script updates the AWS DNS with the local IP address of this machine.
-##  Useful for locating the Pi in a large network like TP.
 
-cd ~/DNS
+# This script updates the AWS DNS with the local IP address of this machine.
+# Useful for locating the Pi in a large network like TP.  This script is
+# called by cron every minute.
+
+# Get the IP address and hostname.
 ipaddress=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 hostname=`cat /etc/hostname`
+request={"hostname":"$hostname","ip":"$ipaddress"}
 
-cat update.template | sed s/HOSTNAME/$hostname/ | sed s/IPADDRESS/$ipaddress/ >update.json
+# Invoke AWS API Gateway to call Lambda function UpdateDNS.
+echo curl -H "Content-Type: application/json" -X POST -d ${request} \
+https://vldi2nta91.execute-api.us-west-2.amazonaws.com/prod/UpdateDNS
 
-aws route53 change-resource-record-sets --hosted-zone-id Z2ZUGCEJOR6L9D --change-batch file://update.json
+curl -H "Content-Type: application/json" -X POST -d ${request} \
+https://vldi2nta91.execute-api.us-west-2.amazonaws.com/prod/UpdateDNS
