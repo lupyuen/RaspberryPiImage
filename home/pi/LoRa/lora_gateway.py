3447cf743f4774484efd48538627383b1db982b7
lupyuen <lupyuen@gmail.com>
2016-06-13 18:27:30 +0800

    Moved LoRa gateway to LoRa folder

--- home/pi/LoRa/lora_gateway.py
+++ home/pi/LoRa/lora_gateway.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# Forward messages from LoRa devices to AWS IoT, and forward AWS IoT messages to LoRa devices.
+# LoRa gateway for AWS IoT.  Forward messages from LoRa devices to AWS IoT, and forward AWS IoT messages to LoRa devices.
+# Based on https://www.cooking-hacks.com/documentation/tutorials/extreme-range-lora-sx1272-module-shield-arduino-raspberry-pi-intel-galileo/
 
 import time
 import datetime
